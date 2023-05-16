66e2b34b263efce2cfd6c45776c490614b68a9ab
Hamza Malik <hamzamalik22258@gmail.com>
2023-05-16 09:10:42 -0700

    Create tag api created

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -13,9 +13,11 @@ from recipe import views
 
 router = DefaultRouter()
 router.register('recipes', views.RecipeViewSet)
+router.register('tags', views.TagViewSet)
 
 app_name = 'recipe'
 
 urlpatterns = [
     path('', include(router.urls)),
+
 ]
