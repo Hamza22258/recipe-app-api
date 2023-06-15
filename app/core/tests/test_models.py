3694e194dc28303f3fef0aa293ed820db2b647e0
Hamza Malik <hamzamalik22258@gmail.com>
2023-06-15 13:11:08 -0700

    Added test and field for image model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -3,6 +3,7 @@ Tests for models.
 """
 
 from decimal import Decimal
+from unittest.mock import patch
 
 from django.test import TestCase
 from django.contrib.auth import get_user_model
@@ -89,3 +90,12 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    @patch('core.models.uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """Test generating image path."""
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, 'example.jpg')
+
+        self.assertEqual(file_path, f'uploads/recipe/{uuid}.jpg')
